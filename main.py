ec276fee84ed64371b5a5770cc1e411c4d20d741
Adam0606069 <horadam2006@gmail.com>
2026-02-06 12:45:48 +0100

    lekeres

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from netmiko import ConnectHandler
 
 device={
-    "host": "192.168.1.1",
+	"port": "22",
+    "host": "192.168.1.5",
     "username": "admin",
     "password": "admin",
     "secret": "admin",
@@ -12,17 +13,18 @@ net_connect=ConnectHandler(**device)
 net_connect.enable()
 
 #config parancsok kezdete
-net_connect.send_config_set(["hostname R13", "banner motd #Jogosulatlan hozzaferes tilos!#"])
+net_connect.send_config_set(["hostname S13", "banner motd #Jogosulatlan hozzaferes tilos!#"])
 output=net_connect.send_command("show running-config")
 print(output)
 
-with open("running_config.txt", "w", encoding="utf-8") as f:
+with open("switch_running_config.txt", "w", encoding="utf-8") as f:
 	f.write(output)
 	
-output2=net_connect.send_command("show running-config | section interface")
+# output2=net_connect.send_command("show running-config | section interface")
+# print(output2)
 
-with open("running_config_interfaces.txt", "w", encoding="utf-8") as f:
-	f.write(output2)
+# with open("switch_running_config_interfaces.txt", "w", encoding="utf-8") as f:
+# 	f.write(output2)
 
 
 #config parancsok vége
