d7ef178cf565d5ff544f67d1eb2d7704f44d26c1
Adam0606069 <horadam2006@gmail.com>
2026-02-06 11:02:59 +0100

    sikereslekeres

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ device={
     "host": "192.168.1.1",
     "username": "admin",
     "password": "admin",
-    "secret": "cisco",
+    "secret": "admin",
     "device_type": "cisco_ios"
 }
 
@@ -12,8 +12,18 @@ net_connect=ConnectHandler(**device)
 net_connect.enable()
 
 #config parancsok kezdete
-net_connect.send_config_set(["hostname R1", "banner motd #Jogosulatlan hozzaferes tilos!#"])
+net_connect.send_config_set(["hostname R13", "banner motd #Jogosulatlan hozzaferes tilos!#"])
 output=net_connect.send_command("show running-config")
 print(output)
+
+with open("running_config.txt", "w", encoding="utf-8") as f:
+	f.write(output)
+	
+output2=net_connect.send_command("show running-config | section interface")
+
+with open("running_config_interfaces.txt", "w", encoding="utf-8") as f:
+	f.write(output2)
+
+
 #config parancsok vége
 net_connect.send_config_set(["end"])
